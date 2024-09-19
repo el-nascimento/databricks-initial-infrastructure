4fa54ab2eb0b21dbc5b7acedeb1c9913c13ed6f1
el-nascimento <lucas.nascimento@qubika.com>
2024-09-19 08:48:28 -0300

    fix: remove unused provider
    Signed-off-by: el-nascimento <lucas.nascimento@qubika.com>

--- modules/databricks_cluster/versions.tf
+++ modules/databricks_cluster/versions.tf
@@ -1,11 +1,6 @@
 terraform {
   required_version = "~> 1.8.1"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.65.0"
-    }
-
     databricks = {
       source = "databricks/databricks"
       version = "~> 1.51.0"
