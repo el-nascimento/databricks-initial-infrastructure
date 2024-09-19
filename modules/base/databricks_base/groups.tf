2c549fc633ade1b57ad19860d3d6b9d0af7065d3
el-nascimento <lucas.nascimento@qubika.com>
2024-09-19 10:28:43 -0300

    fix: rework permission assignment
    - add dependency to metastore assignment
    - remove assignment of individual principal (should be part of Admins
    already)
    
    Signed-off-by: el-nascimento <lucas.nascimento@qubika.com>

--- modules/base/databricks_base/groups.tf
+++ modules/base/databricks_base/groups.tf
@@ -6,24 +6,16 @@ data "databricks_group" "administrators" {
   display_name = "Administrators"
 }
 
-data "databricks_service_principal" "terragrunt" {
-  display_name = "terragrunt"
-}
-
-resource "databricks_mws_permission_assignment" "add_de_group" {
+resource "databricks_mws_permission_assignment" "data_engineers" {
   workspace_id = databricks_mws_workspaces.this.workspace_id
   principal_id = data.databricks_group.data_engineers.id
-  permissions = ["USER"]
+  permissions  = ["USER"]
+  depends_on   = [databricks_metastore_assignment.this]
 }
 
-resource "databricks_mws_permission_assignment" "add_admin_group" {
+resource "databricks_mws_permission_assignment" "administrators" {
   workspace_id = databricks_mws_workspaces.this.workspace_id
   principal_id = data.databricks_group.administrators.id
-  permissions = ["ADMIN"]
-}
-
-resource "databricks_mws_permission_assignment" "add_principal" {
-  workspace_id = databricks_mws_workspaces.this.workspace_id
-  principal_id = data.databricks_service_principal.terragrunt.id
-  permissions = ["ADMIN"]
+  permissions  = ["ADMIN"]
+  depends_on   = [databricks_metastore_assignment.this]
 }
