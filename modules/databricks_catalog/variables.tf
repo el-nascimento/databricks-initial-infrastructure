8a6c00073eb25de1dd363d887301f878550975cb
el-nascimento <lucas.nascimento@qubika.com>
2024-09-19 14:37:54 -0300

    fix: fix catalog storage path
    - conform to how we're handling storage locations
    
    Signed-off-by: el-nascimento <lucas.nascimento@qubika.com>

--- modules/databricks_catalog/variables.tf
+++ modules/databricks_catalog/variables.tf
@@ -31,7 +31,7 @@ data "databricks_group" "administrators" {
 locals {
   prefix          = var.prefix
   catalog_bucket  = "${local.prefix}-catalog"
-  catalog_storage = "s3://${module.bucket.s3_bucket_id}/metastore"
+  catalog_storage = "s3://${module.bucket.s3_bucket_id}/"
   raw_bucket      = "${local.prefix}-raw"
   sandbox_bucket  = "${local.prefix}-sandbox"
   unity_catalog_role_arn = "arn:aws:iam::414351767826:role/unity-catalog-prod-UCMasterRole-14S5ZJVKOTYTL"
