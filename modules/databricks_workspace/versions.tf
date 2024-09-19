36d4504a22314237b35ec462f720924bb3bba1ca
el-nascimento <lucas.nascimento@qubika.com>
2024-09-19 08:47:48 -0300

    feat: move entitlements and groups out of base module
    Signed-off-by: el-nascimento <lucas.nascimento@qubika.com>

--- modules/databricks_workspace/versions.tf
+++ modules/databricks_workspace/versions.tf
@@ -1,16 +1,10 @@
 terraform {
   required_version = "~> 1.8.1"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.65.0"
-    }
-
     databricks = {
       source = "databricks/databricks"
       version = "~> 1.51.0"
     }
-
   }
 }
 
