1dc49b5a213cb067059c1781d27e84c9da6a1d34
el-nascimento <lucas.nascimento@qubika.com>
2024-09-24 22:03:45 -0300

    fix: reorder variables to make sense
    Signed-off-by: el-nascimento <lucas.nascimento@qubika.com>

--- modules/databricks_catalog/variables.tf
+++ modules/databricks_catalog/variables.tf
@@ -33,14 +33,14 @@ data "aws_caller_identity" "current" {}
 locals {
   prefix                          = var.prefix
   catalog_bucket                  = "${local.prefix}-catalog"
-  catalog_storage                 = "s3://${module.bucket.s3_bucket_id}/"
   catalog_access_role_name        = "${local.catalog_bucket}-access"
   catalog_access_role_arn         = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.catalog_access_role_name}"
+  catalog_storage                 = "s3://${module.bucket.s3_bucket_id}/"
+  raw_bucket                      = "${local.prefix}-raw"
   raw_bucket_access_role_name     = "${module.raw_bucket.s3_bucket_id}-access"
   raw_bucket_access_role_arn      = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.raw_bucket_access_role_name}"
+  sandbox_bucket                  = "${local.prefix}-sandbox"
   sandbox_bucket_access_role_name = "${module.sandbox_bucket.s3_bucket_id}-access"
   sandbox_bucket_access_role_arn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.sandbox_bucket_access_role_name}"
-  raw_bucket                      = "${local.prefix}-raw"
-  sandbox_bucket                  = "${local.prefix}-sandbox"
   unity_catalog_role_arn          = "arn:aws:iam::414351767826:role/unity-catalog-prod-UCMasterRole-14S5ZJVKOTYTL"
 }
